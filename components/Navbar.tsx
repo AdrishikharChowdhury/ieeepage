625d4324204bc1bef299a34eefa4fd02cbf47a91
AdrishikharChodhury <amiadrishikhar@gmail.com>
2026-06-05 01:07:03 +0530

    Added Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='w-full px-12 py-6 flex justify-between items-center shadow-md text-md'>
+      <div className="flex items-center gap-2 font-bold text-4xl h-auto">
+        <Image src='/logo.png' alt='logo' width={120} height={100} className='' />
+        <p><span className="font-extrabold text-blue-700" >IEEE</span> STCET SB</p>
+      </div>
+      <ul className='flex gap-6 items-center'>
+        <li>Home</li>
+        <li>Our Story</li>
+        <li>Timeline</li>
+        <li>The Board</li>
+        <li>Highlights</li>
+      </ul>
+      <li className='bg-blue-600 text-white px-6 py-3 rounded-md' >Stories</li>
+    </nav>
+  )
+}
+
+export default Navbar
